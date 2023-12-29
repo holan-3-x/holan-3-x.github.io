dc20e2fbc9e7ae7eb77dd49123bd3aacd8ced638
Holan <65617334+holan-3-x@users.noreply.github.com>
2023-12-30 00:43:24 +0300

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,46 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Silav Radio",
+    artist: "Song by Silav Radio",
+    src: "https://cast1.my-control-panel.com/proxy/silavsil/stream"
+  },
+  {
+    name: "AVA Radio",
+    artist: "Song by AVA Radio",
+    src: "http://212.237.125.209:8181/"
+  },
+  {
+    name: "Rudaw Radio",
+    artist: "Rudaw Radio",
+    src: "https://l3.itworkscdn.net/itwaudio/9006/stream"
+  },
+  {
+    name: "Duhok Radio",
+    artist: "Duhok Radio",
+    src: "http://stream.zeno.fm/kfrrct64hoxuv"
+  },
+  {
+    name: "Waar Radio",
+    artist: "Waar Radio",
+    src: "https://c2.radioboss.fm:18564/stream"
+  },
+
+  {
+    name: "Rudaw Radio",
+    artist: "Rudaw Radio",
+    src: "https://l3.itworkscdn.net/itwaudio/9006/stream"
+  },
+
+
+];
